6e0bfb32c5686f5cb4f67e88b8911b5ab31651a2
michaeltse <spacemichaelhk@gmail.com>
2023-06-03 18:26:13 +0800

    initial spring boot project

---
+++ src/main/java/com/mt/spingbootmall/SpingbootMallApplication.java
@@ -0,0 +1,13 @@
+package com.mt.spingbootmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbootMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingbootMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mt/spingbootmall/SpingbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mt.spingbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingbootMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
